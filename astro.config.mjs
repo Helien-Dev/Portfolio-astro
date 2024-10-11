fe3cf0bb6e7b26f0cff67f8ee7416893494544ca
Alvaro Avila <alvaroaviladev@gmail.com>
2024-10-11 00:23:58 +0000

    Tailwind config done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        server: {
-            watch: { usePolling: true }
-        }
-    }
+  vite: {
+      server: {
+          watch: { usePolling: true }
+      }
+  },
+
+  integrations: [tailwind()]
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx, astro, html}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
